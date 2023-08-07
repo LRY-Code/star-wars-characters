05d10bad71d3ef7677bff9cb6fde3a91c9cec600
Lukas Raymann <lukas.raymann@moguru.de>
2023-08-07 11:45:15 +0200

    init project creation

---
+++ StarWarsCharacters/app/src/main/java/com/example/starwarscharacters/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.starwarscharacters.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue80 = Color(0xFFBCD2FF)
+val BlueGrey80 = Color(0xFFC2CEDC)
+val Turquoise80 = Color(0xFFB8EFE9)
+
+val Blue40 = Color(0xFF506FA4)
+val BlueGrey40 = Color(0xFF5B6271)
+val Turquoise40 = Color(0xFF52797D)
