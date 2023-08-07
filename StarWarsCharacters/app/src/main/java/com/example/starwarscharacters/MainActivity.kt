9da2f2d3cb6700aaf3d45d90f7a65a603c3f85a9
Lukas Raymann <lukas.raymann@moguru.de>
2023-08-07 15:11:10 +0200

    added background image to start screen

--- StarWarsCharacters/app/src/main/java/com/example/starwarscharacters/MainActivity.kt
+++ StarWarsCharacters/app/src/main/java/com/example/starwarscharacters/MainActivity.kt
@@ -17,6 +17,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.starwarscharacters.ui.screens.CharactersScreen
@@ -52,9 +55,10 @@ fun StartScreen(
     onContinueClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .paint(painterResource(R.drawable.background), contentScale = ContentScale.Fit),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
